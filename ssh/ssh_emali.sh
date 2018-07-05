1f33f21c299400e549e3d0d2242bd14c3870b0e7
kissymv <464891809@qq.com>
2018-07-05 17:53:34 +0800

    Update ssh_emali.sh

--- ssh/ssh_emali.sh
+++ ssh/ssh_emali.sh
@@ -1,24 +1,24 @@
 #!/bin/bash
 ###################################################################
-#Script Name    :                                                                                              
+#Script Name    :ssh_emali.sh                                                                                              
 #Description    :                                                                                 
-#Args           :                                                                                                   
-#Author         :tby                                                               
-#Email          :464891809@qq.com                                         
+#Args           :ssh登录sendmail告警                                                                                                   
+#Author         :kissymv                                                              
+#Email          :kissymv@gmail.com                                         
 ###################################################################
 #//IP归属地查询接口
-#$mid = 79482
-#$oid = 16726
-token='2c685341b699d185cc7a5a50dee4fe93'
+#$mid = 
+#$oid = 
+token=' '
 #eval `curl -s "http://api.ip138.com/query/?ip=${SSH_CLIENT%% *}&datatype=jsonp&callback=find" -H "token:$token" | jq . | awk -F':|[ ]+|"' '$3~/^(country|area|region|city|isp)$/{print $3"="$7}'`
 name=`curl -s "http://api.ip138.com/query/?ip=${SSH_CLIENT%% *}&datatype=jsonp&callback=find" -H "token:$token" | awk -F'"' '{print $12,$14,$16,$18,$20,$22}'`
 
 #//邮件配置
 EMAIL='/usr/local/sendEmail-v1.56/sendEmail'
-FEMAIL="464891809@qq.com" #发件邮箱
-MAILP="ilzipplwphffbghd" #发件邮箱密码
+FEMAIL=" " #发件邮箱
+MAILP=" " #发件邮箱密码
 MAILSMTP="smtp.qq.com" #发件邮箱的SMTP
-MAILT="464891809@qq.com" #收件邮箱
+MAILT=" " #收件邮箱
 MAILmessage="登入者IP地址：${SSH_CLIENT%% *}\n\
 用户名：$USER\n\
 登陆时间：time=$(date +%Y-%m-%d_%H:%M:%S)\n\
