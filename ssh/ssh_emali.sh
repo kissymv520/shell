7543ac21ca6d9c28b2b1434ac8d6a3d557dd6664
kissymv <464891809@qq.com>
2018-07-05 17:34:36 +0800

    Update ssh_emali.sh

--- ssh/ssh_emali.sh
+++ ssh/ssh_emali.sh
@@ -1,21 +1,31 @@
 #!/bin/bash
-#########################################################################
-# File Name: WarringLoging.sh
-# Author: LookBack
-# Email: admin#dwhd.org
-# Version:
-# Created Time: Wed 22 Jul 2015 01:41:09 AM CST
-#########################################################################
+###################################################################
+#Script Name    :                                                                                              
+#Description    :                                                                                 
+#Args           :                                                                                                   
+#Author         :tby                                                               
+#Email          :464891809@qq.com                                         
+###################################################################
+#//IP归属地查询接口
+#$mid = 79482
+#$oid = 16726
+token='2c685341b699d185cc7a5a50dee4fe93'
+#eval `curl -s "http://api.ip138.com/query/?ip=${SSH_CLIENT%% *}&datatype=jsonp&callback=find" -H "token:$token" | jq . | awk -F':|[ ]+|"' '$3~/^(country|area|region|city|isp)$/{print $3"="$7}'`
+name=`curl -s "http://api.ip138.com/query/?ip=${SSH_CLIENT%% *}&datatype=jsonp&callback=find" -H "token:$token" | awk -F'"' '{print $12,$14,$16,$18,$20,$22}'`
 
-eval `curl -s "http://ip.taobao.com/service/getIpInfo.php?ip=${SSH_CLIENT%% *}" | jq . | awk -F':|[ ]+|"' '$3~/^(country|area|region|city|isp)$/{print $3"="$7}'`
-
-EMAIL=`which sendEmail`
-FEMAIL="admin#dwhd.org" #发件邮箱
-MAILP="MzU0ODZjOWI0MWU3" #发件邮箱密码
-MAILSMTP="smtp.dwhd.org" #发件邮箱的SMTP
-MAILT="393411264#qq.com" #收件邮箱
+#//邮件配置
+EMAIL='/usr/local/sendEmail-v1.56/sendEmail'
+FEMAIL="464891809@qq.com" #发件邮箱
+MAILP="ilzipplwphffbghd" #发件邮箱密码
+MAILSMTP="smtp.qq.com" #发件邮箱的SMTP
+MAILT="464891809@qq.com" #收件邮箱
 MAILmessage="登入者IP地址：${SSH_CLIENT%% *}\n\
-IP归属地：${country}_${area}_${region}_${city}_${isp}\n\
-被登录服务器IP：$(curl -s ip.cn| grep -Eo '([0-9]{1,3}[\.]){3}[0-9]{1,3}')"
+用户名：$USER\n\
+登陆时间：time=$(date +%Y-%m-%d_%H:%M:%S)\n\
+IP归属地：$name\n\
+服务器IP：hostname=$(hostname)"
+#服务器IP：$(curl -s ip.cn| grep -Eo '([0-9]{1,3}[\.]){3}[0-9]{1,3}')"
+#IP归属地：${country}_${area}_${region}_${city}_${isp}\n\
 
-$EMAIL -q -f $FEMAIL -t $MAILT -u "您服务器有人登录SSH" -m "$MAILmessage" -s $MAILSMTP -o message-charset=utf-8 -xu $FEMAIL -xp $MAILP
+#//邮件发送
+$EMAIL -q -f $FEMAIL -t $MAILT -u "警告!服务器有人登录SSH" -m "$MAILmessage" -s $MAILSMTP -o message-charset=utf-8 -xu $FEMAIL -xp $MAILP
